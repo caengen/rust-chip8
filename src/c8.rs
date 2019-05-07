709102235b7e462890c3d369671bd1f2ac320ba6
Christian Engen <chris.a.engen@gmail.com>
2019-05-07 21:25:10 +0200

    fix struct

--- src/c8.rs
+++ src/c8.rs
@@ -1,24 +1,31 @@
 struct Chip8 {
-  opcode: u16;
-  memory: u8[];
-  rom: u8[];
+  opcode: u16,
+  memory: [u8; 4096],
+  rom: [u8; 4096 - 516],
   // registers V0...VE
-  V: u8[];
+  V: [u8; 16],
   // index register
-  I: u16;
+  I: u16,
   // program counter
-  pc: u16;
+  pc: u16,
   // pixel graphics. 64x34, 2048 pixels total
-  gfx: u8[];
+  gfx: [u8; 64 * 32],
   // timer registers
   // 60 Hz timer registers
-  delayTimer: u16;
-  soundTimer: u16;
+  delayTimer: u8,
+  soundTimer: u8,
   // stack
-  stack: u16[];
-  sp: u8;
+  stack: [u16; 16],
+  sp: u8,
 
   // key state
-  keys: u8[];
-  debug: bool;
+  keys: [u8; 16],
+  debug: bool
+}
+
+impl Chip8 {
+  fn initialize() {
+
+  }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod c8;
 fn main() {
+    let c8 = Chip8 { }
     println!("Hello, world!");
 }
